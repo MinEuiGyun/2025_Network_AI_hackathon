5e97e3d6e2aedd9cbad13abfa81302589c6221ff
dnxo1224 <dnxo1224@naver.com>
2025-08-07 10:56:38 +0900

    feat: TimesNet

---
+++ Task1/layers/Conv_Blocks.py
@@ -0,0 +1,4 @@
+# Dummy Inception_Block_V1 class placeholder
+class Inception_Block_V1:
+    def __init__(self, in_channels, out_channels, num_kernels=8): pass
+    def __call__(self, x): return x
---
+++ Task1/layers/Embed.py
@@ -0,0 +1,4 @@
+# Dummy DataEmbedding class placeholder
+class DataEmbedding:
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, x, x_mark): return x
